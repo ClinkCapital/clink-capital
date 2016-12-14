b7d4a2cf557d7f4bb687b07bbb454e4ceb45d861
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-14 13:56:27 -0600

    key secret

--- clink/settings.py
+++ clink/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wjv)b=ij$q3s5$ze&n$4=x6ar2#i7pl(cedr7a197m6k)@_&-q'
+SECRET_KEY = os.getenv('CLINK_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
