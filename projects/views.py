7dc6c5fc748fd34def9de6093cc023456ff03e25
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-14 23:29:35 -0600

    Creado esqueleto base para los templates

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 from django.shortcuts import render
 
 
+PAGE_TITLE = 'Clink|Capital'
+
+
 def index(request):
-    return HttpResponse('Hola')
+    template = 'index.html'
+    context = {
+        'title': PAGE_TITLE
+    }
+    return render(request, template, context)
