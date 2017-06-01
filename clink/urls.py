6553b3176c207f92b419c70c3dfa128317ef1547
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-06-01 17:55:40 -0500

    refactored static files and settings. Algo added new requirements

--- clink/urls.py
+++ clink/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls)), ]
