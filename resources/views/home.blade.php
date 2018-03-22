d57f06a9765732cad49b412e1bdb24f8efdd0ea9
Ramses Martinez <ramses.mtz96@gmail.com>
2018-03-22 01:56:53 -0600

    migrate projecto fron django to laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  <link rel="icon" href="/media/images/temporal-ico.ico">
+  <link rel="stylesheet" href="/css/app.css" />
+  <link rel="stylesheet" href="/css/font-awesome.min.css" />
+
+  <title>Clink Capital</title>
+</head>
+<body>
+  <header id="header">
+  <h1 id="logo">
+    <a href="index.html">
+      <div id="content-title">
+        <img id="img-nav" src="/images/clinkraterizado.png" alt="Cink | Capital">
+        <p></p>
+      </div>
+    </a>
+  </h1>
+
+  <nav id="nav">
+    <ul>
+      <li><a class="scrolly" href="#objetivo">Objetivo</a></li>
+      <li><a class="scrolly" href="#que-hacemos">¿Qué hacemos?</a>
+      </li>
+      <li><a href="#proyectos" class="button special scrolly">Nuestro Portafolio</a></li>
+    </ul>
+  </nav>
+</header>
+
+  <main>
+    <section id="banner">
+      <div class="content">
+        <header>
+          <h2><span class="enfasis">CONECTANDO</span> GRANDES IDEAS</h2>
+        </header>
+        <span class="image"><img src="/images/clinkraterizado-c.png" alt="" /></span>
+      </div>
+      <a href="#objetivo" class="goto-next scrolly">Next</a>
+    </section>
+
+    <!-- Objetivo -->
+    <section id="objetivo" class="spotlight style1 bottom">
+      <span class="image fit main"><img src="/images/banner_2.jpg" alt="" /></span>
+      <div class="content">
+        <div class="container">
+          <div class="row">
+            <div class="3u 12u$(medium)">
+              <header>
+                <h2>Nos enfocamos</h2>
+              </header>
+            </div>
+            <div class="9u 12u$(medium)">
+              <p>
+                en buscar capital, para darle crecimiento y acelaración a PYMES, que tienen un proyecto <strong>innovador.</strong>
+                orientado principalmente, en sectores de <strong>tecnología, telecomunicaciones, internet y energías renovables.</strong>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="#que-hacemos" class="goto-next scrolly">Next</a>
+    </section>
+
+    <!-- ¿Qué hacemos? -->
+    <section id="que-hacemos" class="spotlight style2 right">
+      <span class="image fit main"><img src="/images/banner_3.jpg" alt="" /></span>
+      <div class="content">
+        <header>
+          <h2>¿Qué hacemos?</h2>
+        </header>
+        <ul>
+          <li>Conectamos y enlazamos el capital de los inversionistas con los empleadores.</li>
+          <li>Democratizamos la inversión al público inversionista a través del <strong>crowdfunding.</strong></li>
+          <li>Informamos a los inversionistas de las mejores opciones de inversión para su capital.</li>
+          <li>Gestionamos y apoyamos a los emprendedores y a las PYMES a <strong>maximizar sus ideas y sus recursos</strong>.</li>
+        </ul>
+        <ul class="actions">
+          <li><a href="#" class="button">Conoce más</a></li>
+        </ul>
+      </div>
+      <a href="#proyectos" class="goto-next scrolly">Next</a>
+    </section>
+
+    <!-- Projectos -->
+    <section id="proyectos" class="wrapper style1 special fade-up">
+      <div class="container">
+        <header class="major">
+          <h2>Nuestro Portafolio</h2>
+        </header>
+        <div class="box alt">
+          <div class="row uniform">
+            <section class="4u 6u(medium) 12u$(xsmall)">
+              <figure class="logo-client">
+                <img src="/images/probionics.png" alt="">
+                <img src="https://userscontent2.emaze.com/images/e85e73bb-ce7e-4ffd-b778-6e2b44c10053/ecfaecd31b58078193cb0fc0d58cd214.png" alt="">
+              </figure>
+              <p>
+                Descripción.
+              </p>
+            </section>
+            <!--
+            <section class="4u 6u$(medium) 12u$(xsmall)">
+              <span class="icon alt major fa-comment"></span>
+              <h3>Eleifend lorem ornare</h3>
+              <p>
+                Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.
+              </p>
+            </section>
+            <section class="4u$ 6u(medium) 12u$(xsmall)">
+              <span class="icon alt major fa-flask"></span>
+              <h3>Cubilia cep lobortis</h3>
+              <p>
+                Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.
+              </p>
+            </section>
+            <section class="4u 6u$(medium) 12u$(xsmall)">
+              <span class="icon alt major fa-paper-plane"></span>
+              <h3>Non semper interdum</h3>
+              <p>
+                Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.
+              </p>
+            </section>
+            <section class="4u 6u(medium) 12u$(xsmall)">
+              <span class="icon alt major fa-file"></span>
+              <h3>Odio laoreet accumsan</h3>
+              <p>
+                Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.
+              </p>
+            </section>
+            <section class="4u$ 6u$(medium) 12u$(xsmall)">
+              <span class="icon alt major fa-lock"></span>
+              <h3>Massa arcu accumsan</h3>
+              <p>
+                Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.
+              </p>
+            </section>
+            -->
+          </div>
+        </div>
+        <footer class="major">
+          <ul class="actions">
+            <li><a href="#" class="button">Más detalles</a></li>
+          </ul>
+        </footer>
+      </div>
+    </section>
+
+    <!-- Five -->
+    {{--  <section id="five" class="wrapper style2 special fade">
+      <div class="container">
+        <header>
+          <h2>Contáctanos</h2>
+          <p>
+            Envianos tu email y a la brevedad nos pondremos en contacto.
+          </p>
+        </header>
+        <form method="post" action="#" class="container 80%">
+          <div class="row uniform 80%">
+            <div class="8u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Tu Email" /></div>
+            <div class="4u$ 12u$(xsmall)"><input type="submit" value="Enviar" class="fit special" /></div>
+          </div>
+        </form>
+      </div>
+    </section>  --}}
+  </main>  
+
+  <!-- Scripts -->
+  <script src="/js/jquery.min.js"></script>
+  <script src="/js/jquery.scrolly.min.js"></script>
+  <script src="/js/jquery.dropotron.min.js"></script>
+  <script src="/js/jquery.scrollex.min.js"></script>
+  <script src="/js/skel.min.js"></script>
+  <script src="/js/util.js"></script>
+  <script src="/js/main.js"></script>
+</body>
+</html>
